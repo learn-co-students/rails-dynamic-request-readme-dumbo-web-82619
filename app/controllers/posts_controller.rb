c9f144bb1e45015d9bb4cbd2e69c7c0877f4a421
Karina Macancela <karymacancela@gmail.com>
2019-09-23 15:35:16 -0400

    Done.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,11 @@
+class PostsController <ApplicationController
+
+    def show
+        @post = Post.find(params[:id])
+    end
+
+    def index
+        @posts = Post.all
+    end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.destroy_all
+
+p1 = Post.create(title: "How to Code in Ruby", description: "Some description")
+p2 = Post.create(title: "How to Code in Java", description: "Some description")
+p3 = Post.create(title: "How to Code in JavaScript", description: "Some description")
+p4 = Post.create(title: "How to Code in Swift", description: "Some description")
+p5 = Post.create(title: "How to Code in Python", description: "Some description")
+
+puts "It has been seeded!"
